1058195630fe9bbaed37b40b8659f79109ce799b
Jonathan Kelman <kelman.jonathan@gmail.com>
2025-05-26 17:58:52 +0300

    feat: added error middleware and global error class

--- backend/src/controllers/cpuMetrics.controller.ts
+++ backend/src/controllers/cpuMetrics.controller.ts
@@ -1,9 +1,15 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
+import { AppError } from '../appErrorClass'
 
-const getCpuMetrics = async (req: Request, res: Response) => {
-	res.send('hero')
+const getCpuMetrics = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		throw new Error('testing error')
+		res.send('hero')
+	} catch (error: any) {
+		next(new AppError(error.message || 'Error fetching metrics', error.status))
+	}
 }
 
-const cpuMetricsController = {getCpuMetrics}
+const cpuMetricsController = { getCpuMetrics }
 
 export default cpuMetricsController
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cpuMetricsRouter from './routes/cpuMetrics.route'
+import errorHandler from './errorHandler'
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,9 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 
 app.use('/api/cpuMetric', cpuMetricsRouter)
+
+// error middleware 
+app.use(errorHandler)
 
 app.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`)
