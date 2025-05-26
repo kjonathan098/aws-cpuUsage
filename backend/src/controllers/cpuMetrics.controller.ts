02b083a6d0abec145147b71d10c4e2ae0ba8594b
Jonathan Kelman <kelman.jonathan@gmail.com>
2025-05-26 18:03:10 +0300

    feat: added global success response function

--- backend/src/controllers/cpuMetrics.controller.ts
+++ backend/src/controllers/cpuMetrics.controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import { AppError } from '../appErrorClass'
+import { respondSuccess } from '../utils/respondSuccess'
 
 const getCpuMetrics = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		throw new Error('testing error')
-		res.send('hero')
+		respondSuccess(res, 'hero')
 	} catch (error: any) {
 		next(new AppError(error.message || 'Error fetching metrics', error.status))
 	}
